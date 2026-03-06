87203e37b1ee7ebf9c3bd2f80ed0aa97b9bbf05c
Kim Duer <kimcodesjs.com>
2026-03-05 23:01:57 -0600

    init project w/ Vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Pages/Landing/Landing";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
